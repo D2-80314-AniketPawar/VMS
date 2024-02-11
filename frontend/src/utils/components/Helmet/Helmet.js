e258249a65afdc0e9f7384d2a5cf32cd40353c5f
aniketpawar1701 <aniketpawar170101@gmail.com>
2024-02-11 15:04:39 +0530

    frontend updated

--- frontend/src/utils/components/Helmet/Helmet.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Helmet = (props) => {
-  document.title = "Book Car - " + props.title;
-  return <div className="w-100">{props.children}</div>;
-};
-
-export default Helmet;
